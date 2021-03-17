2b5f7936754fc0ada5cd60598baee186def8a5e3
Alexandre Pereira <alexandre.gomes2606@gmail.com>
2021-03-16 23:22:31 -0300

    Adjust damage types formatting

--- hunter/src/main/kotlin/br/alexandregpereira/hunter/SearchDamageType.kt
+++ hunter/src/main/kotlin/br/alexandregpereira/hunter/SearchDamageType.kt
@@ -6,7 +6,6 @@ import kotlinx.serialization.decodeFromString
 suspend fun main() = start {
     json.decodeFromString<List<Monster>>(readJsonFile(JSON_FILE_NAME))
         .asSequence()
-        .filter { it.damageResistances.isNotEmpty() }
         .map { it.damageResistances + it.damageImmunities + it.damageVulnerabilities }
         .reduce { acc, list -> acc + list }
         .toSet()
