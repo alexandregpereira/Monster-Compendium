921f1fa77214ecc69f82e434bc11b494290875bf
Alexandre Pereira <alexandre.pereira@picpay.com>
2021-03-21 19:33:36 -0300

    Adjust code

--- ui/src/main/kotlin/br/alexandregpereira/hunter/ui/compose/MonsterImage.kt
+++ ui/src/main/kotlin/br/alexandregpereira/hunter/ui/compose/MonsterImage.kt
@@ -18,7 +18,6 @@ package br.alexandregpereira.hunter.ui.compose
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
