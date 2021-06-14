6bc1e0cbe1af93e4232e172f70ee5ac2d08cd472
Alexandre Pereira <alexandre.gomes2606@gmail.com>
2021-06-14 13:20:51 -0300

    Save images to images.json until page 89

--- hunter/src/main/kotlin/br/alexandregpereira/hunter/scripts/GetImagesFromWeb.kt
+++ hunter/src/main/kotlin/br/alexandregpereira/hunter/scripts/GetImagesFromWeb.kt
@@ -23,24 +23,25 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.reduce
+import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import org.jsoup.Jsoup
 
-private const val TOTAL_PAGES = 26
+private const val TOTAL_PAGES = 97
 private const val URL =
-    "https://www.dndbeyond.com/monsters?filter-search=&filter-source=5&filter-source-category=1&filter-type=0&page="
+    "https://www.dndbeyond.com/monsters?page="
 private const val FIRST_URL =
-    "https://www.dndbeyond.com/monsters?filter-type=0&filter-search=&filter-cr-min=&filter-cr-max=&filter-armor-class-min=&filter-armor-class-max=&filter-average-hp-min=&filter-average-hp-max=&filter-is-legendary=&filter-is-mythic=&filter-has-lair=&filter-source-category=1&filter-source=5"
+        "https://www.dndbeyond.com/monsters"
 
 @FlowPreview
 @ExperimentalCoroutinesApi
 suspend fun main() {
-    (1..TOTAL_PAGES).asSequence().asFlow()
+    (90..TOTAL_PAGES).asSequence().asFlow()
         .flatMapConcat { page ->
             getImages(page)
         }.map { pairs ->
@@ -50,24 +51,30 @@ suspend fun main() {
                     url = it.second
                 )
             }
-        }
-        .reduce { acc, value -> acc + value }
-        .onEach {
-            println(it)
-        }
-        .let { images ->
+        }.onEach { images ->
             val oldImages = json.decodeFromString<HashSet<WebImageData>>(readJsonFile(JSON_IMAGES_FILE_NAME))
-            println(oldImages)
+
+            println("newImages")
+            images.forEach { println(it) }
+            println()
+
             oldImages.addAll(images.toHashSet())
-            saveJsonFile(oldImages.sortedBy { it.name }, JSON_IMAGES_FILE_NAME)
+            saveJsonFile(oldImages.sortedBy { it.name }, JSON_IMAGES_FILE_NAME, printJson = false)
+
+            val minutes: Long = 1000 * 60 * 1 * 4
+            println("Waiting $minutes minutes")
+            delay(minutes)
         }
+        .catch {
+            println(it.toString())
+        }
+        .collect()
 }
 
 @Suppress("BlockingMethodInNonBlockingContext")
 private fun getImages(page: Int): Flow<List<Pair<String, String>>> {
     val url = if (page == 1) FIRST_URL else URL + page
     return flow {
-        delay(1000 * 60 * 1)
         val referer = if (page == 1) "https://www.dndbeyond.com/monsters" else URL + (page - 1)
         println("Getting: $url")
         println("Referer: $referer")
@@ -81,25 +88,6 @@ private fun getImages(page: Int): Flow<List<Pair<String, String>>> {
         elements.map {
             it.parent().parent().attr("data-slug") to it.attr("href")
         }
-    }.catch {
-        println(it.toString())
-        emptyList<String>()
-    }
-}
-
-private fun test(page: Int): Flow<List<Pair<String, String>>> {
-    return flow {
-        println("Getting: $page")
-        emit(page)
-    }.map { url2 ->
-        val elements = listOf(url2)
-        println("Elements founded: ${elements.size}")
-        elements.map {
-            "a" to it.toString()
-        }
-    }.catch {
-        println(it.message)
-        emptyList<String>()
     }
 }
 
@@ -112,7 +100,6 @@ private fun getHeaders(referer: String): Map<String, String> {
         "user-agent" to "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
     )
 }
-
 
 @Serializable
 data class WebImageData(
