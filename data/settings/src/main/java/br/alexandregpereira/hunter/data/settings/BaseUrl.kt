0c3e328987ebb70bb185e97204d5dd2d3dbbd167
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2022-10-15 18:22:13 -0300

    Change base url

--- data/settings/src/main/java/br/alexandregpereira/hunter/data/settings/BaseUrl.kt
+++ data/settings/src/main/java/br/alexandregpereira/hunter/data/settings/BaseUrl.kt
@@ -1,4 +1,4 @@
 package br.alexandregpereira.hunter.data.settings
 
 const val DEFAULT_API_BASE_URL =
-    "https://raw.githubusercontent.com/alexandregpereira/hunter-api/1.0/json/"
+    "https://raw.githubusercontent.com/alexandregpereira/hunter-api/main/json/"
