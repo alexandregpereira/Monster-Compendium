8154a3605c7fb723f654bf86cb543049c4559de9
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2022-10-16 15:19:42 -0300

    Update copyrights

--- domain/monster/src/main/kotlin/br/alexandregpereira/hunter/domain/monster/spell/model/Spellcasting.kt
+++ domain/monster/src/main/kotlin/br/alexandregpereira/hunter/domain/monster/spell/model/Spellcasting.kt
@@ -1,3 +1,20 @@
+/*
+ * Hunter - DnD 5th edition monster compendium application
+ * Copyright (C) 2022. Alexandre Gomes Pereira.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package br.alexandregpereira.hunter.domain.monster.spell.model
 
 data class Spellcasting(
