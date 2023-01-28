41f0595d72ce22cf5c378f76e570b73f74319c32
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2023-01-27 21:21:58 -0300

    Created shared module and ios app folder (#113)

---
+++ iosApp/MonsterCompendium/MonsterCompendiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterCompendiumApp.swift
+//  MonsterCompendium
+//
+//  Created by Alexandre G Pereira on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MonsterCompendiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
