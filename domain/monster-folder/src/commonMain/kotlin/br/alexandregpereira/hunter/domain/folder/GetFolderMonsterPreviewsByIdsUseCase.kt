d75f8e60539547aa1abf6e8fb5bd920fb49eb6b5
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2023-01-27 22:10:59 -0300

    Transform domain modules into kmm modules (#114)

---
+++ domain/monster-folder/src/commonMain/kotlin/br/alexandregpereira/hunter/domain/folder/GetFolderMonsterPreviewsByIdsUseCase.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Alexandre Gomes Pereira
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.alexandregpereira.hunter.domain.folder
+
+import br.alexandregpereira.hunter.domain.folder.model.MonsterPreviewFolder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class GetFolderMonsterPreviewsByIdsUseCase(
+    private val repository: FolderMonsterPreviewRepository
+) {
+
+    operator fun invoke(indexes: List<String>): Flow<List<MonsterPreviewFolder>> {
+        return repository.getFolderMonsterPreviewsByIds(indexes).map { monsters ->
+            val aggregator = mutableListOf<MonsterPreviewFolder>()
+            indexes.forEach { monsterIndex ->
+                aggregator.add(
+                    monsters.find { it.index == monsterIndex }
+                        ?: throw RuntimeException("$monsterIndex not found")
+                )
+            }
+            aggregator
+        }
+    }
+}
