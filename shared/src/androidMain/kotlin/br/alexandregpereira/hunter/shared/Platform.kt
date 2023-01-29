13c78a035019cc75064ef8907f8e96ddefbd662f
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2023-01-29 18:46:23 -0300

    Fix broken build

--- shared/src/androidMain/kotlin/br/alexandregpereira/hunter/shared/Platform.kt
+++
@@ -1,7 +0,0 @@
-package br.alexandregpereira.hunter.shared
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
