1271b0322216ef2d217374f13b7f76afcd3f453c
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2024-02-16 08:48:43 -0300

    Adjust GoToCompendiumIndex code

--- feature/monster-compendium/android/src/main/kotlin/br/alexandregpereira/hunter/monster/compendium/MonsterCompendiumFeature.kt
+++ feature/monster-compendium/android/src/main/kotlin/br/alexandregpereira/hunter/monster/compendium/MonsterCompendiumFeature.kt
@@ -18,14 +18,8 @@ package br.alexandregpereira.hunter.monster.compendium
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.dp
-import br.alexandregpereira.hunter.monster.compendium.state.MonsterCompendiumAction.GoToCompendiumIndex
 import br.alexandregpereira.hunter.monster.compendium.ui.MonsterCompendiumScreen
 import org.koin.androidx.compose.koinViewModel
 
@@ -34,22 +28,11 @@ fun MonsterCompendiumFeature(
     contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
     val viewModel: MonsterCompendiumViewModel = koinViewModel()
-    var compendiumIndex by remember {
-        mutableStateOf(-1)
-    }
     MonsterCompendiumScreen(
         state = viewModel.state.collectAsState().value,
+        actionHandler = viewModel,
         initialScrollItemPosition = viewModel.initialScrollItemPosition,
-        compendiumIndex = compendiumIndex,
         contentPadding = contentPadding,
         events = viewModel,
     )
-
-    LaunchedEffect(key1 = Unit) {
-        viewModel.action.collect { action ->
-            when (action) {
-                is GoToCompendiumIndex -> compendiumIndex = action.index
-            }
-        }
-    }
 }
