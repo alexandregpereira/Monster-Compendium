09b2a7300896a21eb4ca84f8bdc0ef628c6c5456
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2024-07-19 18:32:11 -0300

    Fix jvm migration and sync monster after imported creatures

--- domain/app/data/src/jvmMain/kotlin/br/alexandregpereira/hunter/data/di/JvmSqlDriverFactory.kt
+++ domain/app/data/src/jvmMain/kotlin/br/alexandregpereira/hunter/data/di/JvmSqlDriverFactory.kt
@@ -23,13 +23,45 @@ import org.koin.core.scope.Scope
 import java.io.File
 
 internal actual fun Scope.createSqlDriver(): SqlDriver {
+    val databasePath = getDatabasePath()
+    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$databasePath")
+
+    val currentVersion = driver.getDatabaseVersion()
+    val schemaVersion = Database.Schema.version
+    if (schemaVersion > currentVersion) {
+        Database.Schema.migrate(driver, currentVersion, schemaVersion)
+        driver.setDatabaseVersion(schemaVersion)
+        println("init: migrated from $currentVersion to $schemaVersion")
+    } else {
+        Database.Schema.create(driver)
+    }
+
+    return driver
+}
+
+private fun getDatabasePath(): String {
     val userFolder = System.getProperty("user.home")
     val appDataFolder = File(userFolder, ".monster-compendium")
     if (appDataFolder.exists().not()) {
         appDataFolder.mkdirs()
     }
     val databasePath = File(appDataFolder, "hunter-database.db")
-    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:${databasePath.absolutePath}")
-    Database.Schema.create(driver)
-    return driver
+    return databasePath.absolutePath
+}
+
+private fun SqlDriver.getDatabaseVersion(): Int {
+    val sqlCursor = this.executeQuery(
+        identifier = null,
+        sql = "PRAGMA user_version;",
+        parameters = 0,
+        binders = null
+    )
+
+    val initialDatabaseVersion = 25
+    return sqlCursor.getLong(0)?.toInt()
+        ?.takeUnless { it == 0 } ?: initialDatabaseVersion
+}
+
+private fun SqlDriver.setDatabaseVersion(version: Int) {
+    this.execute(null, "PRAGMA user_version = $version;", 0, null)
 }
