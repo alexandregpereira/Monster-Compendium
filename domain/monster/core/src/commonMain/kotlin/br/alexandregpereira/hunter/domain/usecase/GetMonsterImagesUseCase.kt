3adc1cb7d82b81c1723248239040ee36223115ce
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2025-05-17 23:23:39 -0300

    Replace old image url to the new image url (#361)

--- domain/monster/core/src/commonMain/kotlin/br/alexandregpereira/hunter/domain/usecase/GetMonsterImagesUseCase.kt
+++ domain/monster/core/src/commonMain/kotlin/br/alexandregpereira/hunter/domain/usecase/GetMonsterImagesUseCase.kt
@@ -40,7 +40,7 @@ class GetMonsterImagesUseCase(
 
     companion object {
         private const val DEFAULT_MONSTER_IMAGES_JSON_URL =
-            "https://raw.githubusercontent.com/alexandregpereira/hunter-api/main/json/" +
+            "https://raw.githubusercontent.com/alexandregpereira/Monster-Compendium-Content/main/json/" +
                     "monster-images.json"
     }
 }
