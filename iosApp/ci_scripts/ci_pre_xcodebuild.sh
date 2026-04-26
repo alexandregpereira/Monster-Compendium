9054272c3481ded57c5a22edddbe3cb5a6bea518
Alexandre G Pereira <alexandre.gomes2606@gmail.com>
2026-04-26 12:42:37 -0300

    Install Java dependency in Xcode pre-build script
    - Add `temurin@17` installation via Homebrew to `ci_pre_xcodebuild.sh` to support Gradle during the Kotlin Multiplatform `embedAndSignAppleFrameworkForXcode` build phase.

--- iosApp/ci_scripts/ci_pre_xcodebuild.sh
+++ iosApp/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# Install Java for Gradle (required by the KMP embedAndSignAppleFrameworkForXcode build phase)
+brew install --cask temurin@17
+
+# Install CocoaPods and fetch dependencies
 cd "$(dirname "$0")/.."
 brew install cocoapods
 pod install
